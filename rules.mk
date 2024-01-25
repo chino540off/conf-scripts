6e6cc1f3b5be410150df01156726f76ba69d7897
Olivier Détour <odetour@antidot.net>
2024-01-25 11:07:55 +0100

    Add passmenu and pushover scripts

--- rules.mk
+++ rules.mk
@@ -7,7 +7,11 @@ $(_m)-dir-bin	= ~/local/bin
 
 $(_m)-links =	\
 	sp \
+	passmenu \
+	po \
 	pb
 
 $(_m)-link-sp	= ~/local/bin/sp
+$(_m)-link-passmenu	= ~/local/bin/passmenu
+$(_m)-link-po	= ~/local/bin/po
 $(_m)-link-pb	= ~/local/bin/pb
